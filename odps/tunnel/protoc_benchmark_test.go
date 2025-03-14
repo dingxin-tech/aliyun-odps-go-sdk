19174b6fb7d5b50c048aeca76703272ca3d44bbb
鼎昕 <zhangdingxin.zdx@alibaba-inc.com>
2025-03-14 16:00:36 +0800

    BenchmarkWriteBytes part2

--- odps/tunnel/protoc_benchmark_test.go
+++ odps/tunnel/protoc_benchmark_test.go
@@ -9,26 +9,6 @@ import (
 	"google.golang.org/protobuf/encoding/protowire"
 )
 
-// BenchmarkWriteBytes
-// BenchmarkWriteBytes/Old_Small
-// BenchmarkWriteBytes/Old_Small-8         	15771470	        72.60 ns/op	     112 B/op	       4 allocs/op
-// BenchmarkWriteBytes/New_Small
-// BenchmarkWriteBytes/New_Small-8         	58773110	        19.97 ns/op	       8 B/op	       1 allocs/op
-// BenchmarkWriteBytes/New2_Small
-// BenchmarkWriteBytes/New2_Small-8        	36471883	        32.51 ns/op	      24 B/op	       1 allocs/op
-// BenchmarkWriteBytes/Old_1KB
-// BenchmarkWriteBytes/Old_1KB-8           	16531686	        70.70 ns/op	     112 B/op	       4 allocs/op
-// BenchmarkWriteBytes/New_1KB
-// BenchmarkWriteBytes/New_1KB-8           	58092157	        19.92 ns/op	       8 B/op	       1 allocs/op
-// BenchmarkWriteBytes/New2_1KB
-// BenchmarkWriteBytes/New2_1KB-8          	35389356	        32.52 ns/op	      24 B/op	       1 allocs/op
-// BenchmarkWriteBytes/Old_1KB#01
-// BenchmarkWriteBytes/Old_1KB#01-8        	16017529	        72.76 ns/op	     112 B/op	       4 allocs/op
-// BenchmarkWriteBytes/New_1KB#01
-// BenchmarkWriteBytes/New_1KB#01-8        	57669663	        20.17 ns/op	       8 B/op	       1 allocs/op
-// BenchmarkWriteBytes/New2_1KB#01
-// BenchmarkWriteBytes/New2_1KB#01-8       	36332670	        32.85 ns/op	      24 B/op	       1 allocs/op
-
 // 旧版本实现（用于对比）
 type OldProtocStreamWriter struct {
 	inner io.Writer
@@ -238,17 +218,80 @@ func runBenchmark(b *testing.B, writer interface {
 }
 
 // 新旧实现对比基准测试
+// BenchmarkOldImplementation
+// BenchmarkOldImplementation/Varint_Small
+// BenchmarkOldImplementation/Varint_Small-8         	34086914	        34.28 ns/op	      49 B/op	       2 allocs/op
+// BenchmarkOldImplementation/Varint_Medium
+// BenchmarkOldImplementation/Varint_Medium-8        	23941263	        50.77 ns/op	      64 B/op	       3 allocs/op
+// BenchmarkOldImplementation/Varint_Large
+// BenchmarkOldImplementation/Varint_Large-8         	19006237	        54.12 ns/op	      64 B/op	       3 allocs/op
+// BenchmarkOldImplementation/Fixed32
+// BenchmarkOldImplementation/Fixed32-8              	32886622	        35.90 ns/op	      52 B/op	       2 allocs/op
+// BenchmarkOldImplementation/Fixed64
+// BenchmarkOldImplementation/Fixed64-8              	33470197	        33.28 ns/op	      56 B/op	       2 allocs/op
+// BenchmarkOldImplementation/WriteBytes_Small
+// BenchmarkOldImplementation/WriteBytes_Small-8     	17467216	        66.81 ns/op	     104 B/op	       4 allocs/op
 func BenchmarkOldImplementation(b *testing.B) {
 	oldWriter := &OldProtocStreamWriter{inner: io.Discard}
 	runBenchmark(b, oldWriter)
 }
 
+// BenchmarkNewImplementation
+// BenchmarkNewImplementation/Varint_Small
+// BenchmarkNewImplementation/Varint_Small-8         	66530526	        17.62 ns/op	       8 B/op	       1 allocs/op
+// BenchmarkNewImplementation/Varint_Medium
+// BenchmarkNewImplementation/Varint_Medium-8        	69088772	        17.39 ns/op	       8 B/op	       1 allocs/op
+// BenchmarkNewImplementation/Varint_Large
+// BenchmarkNewImplementation/Varint_Large-8         	59353785	        19.72 ns/op	       8 B/op	       1 allocs/op
+// BenchmarkNewImplementation/Fixed32
+// BenchmarkNewImplementation/Fixed32-8              	73491507	        16.10 ns/op	       8 B/op	       1 allocs/op
+// BenchmarkNewImplementation/Fixed64
+// BenchmarkNewImplementation/Fixed64-8              	70767063	        17.64 ns/op	       8 B/op	       1 allocs/op
+// BenchmarkNewImplementation/WriteBytes_Small
+// BenchmarkNewImplementation/WriteBytes_Small-8     	39911085	        29.25 ns/op	      16 B/op	       2 allocs/op
 func BenchmarkNewImplementation(b *testing.B) {
 	newWriter := &NewProtocStreamWriter{inner: io.Discard}
 	runBenchmark(b, newWriter)
 }
 
+// BenchmarkNew2Implementation
+// BenchmarkNew2Implementation/Varint_Small
+// BenchmarkNew2Implementation/Varint_Small-8         	31800223	        33.69 ns/op	      24 B/op	       1 allocs/op
+// BenchmarkNew2Implementation/Varint_Medium
+// BenchmarkNew2Implementation/Varint_Medium-8        	29627342	        34.20 ns/op	      24 B/op	       1 allocs/op
+// BenchmarkNew2Implementation/Varint_Large
+// BenchmarkNew2Implementation/Varint_Large-8         	32771252	        35.26 ns/op	      24 B/op	       1 allocs/op
+// BenchmarkNew2Implementation/Fixed32
+// BenchmarkNew2Implementation/Fixed32-8              	36227144	        33.78 ns/op	      24 B/op	       1 allocs/op
+// BenchmarkNew2Implementation/Fixed64
+// BenchmarkNew2Implementation/Fixed64-8              	33905568	        31.97 ns/op	      24 B/op	       1 allocs/op
+// BenchmarkNew2Implementation/WriteBytes_Small
+// BenchmarkNew2Implementation/WriteBytes_Small-8     	21873462	        50.50 ns/op	      29 B/op	       2 allocs/op
+func BenchmarkNew2Implementation(b *testing.B) {
+	newWriter := &NewProtocStreamWriter2{inner: io.Discard}
+	runBenchmark(b, newWriter)
+}
+
 // WriteBytes 专项测试
+// BenchmarkWriteBytes
+// BenchmarkWriteBytes/Old_Small
+// BenchmarkWriteBytes/Old_Small-8         	15771470	        72.60 ns/op	     112 B/op	       4 allocs/op
+// BenchmarkWriteBytes/New_Small
+// BenchmarkWriteBytes/New_Small-8         	58773110	        19.97 ns/op	       8 B/op	       1 allocs/op
+// BenchmarkWriteBytes/New2_Small
+// BenchmarkWriteBytes/New2_Small-8        	36471883	        32.51 ns/op	      24 B/op	       1 allocs/op
+// BenchmarkWriteBytes/Old_1KB
+// BenchmarkWriteBytes/Old_1KB-8           	16531686	        70.70 ns/op	     112 B/op	       4 allocs/op
+// BenchmarkWriteBytes/New_1KB
+// BenchmarkWriteBytes/New_1KB-8           	58092157	        19.92 ns/op	       8 B/op	       1 allocs/op
+// BenchmarkWriteBytes/New2_1KB
+// BenchmarkWriteBytes/New2_1KB-8          	35389356	        32.52 ns/op	      24 B/op	       1 allocs/op
+// BenchmarkWriteBytes/Old_1KB#01
+// BenchmarkWriteBytes/Old_1KB#01-8        	16017529	        72.76 ns/op	     112 B/op	       4 allocs/op
+// BenchmarkWriteBytes/New_1KB#01
+// BenchmarkWriteBytes/New_1KB#01-8        	57669663	        20.17 ns/op	       8 B/op	       1 allocs/op
+// BenchmarkWriteBytes/New2_1KB#01
+// BenchmarkWriteBytes/New2_1KB#01-8       	36332670	        32.85 ns/op	      24 B/op	       1 allocs/op
 func BenchmarkWriteBytes(b *testing.B) {
 	sizes := []int{
 		128,       // 小数据
